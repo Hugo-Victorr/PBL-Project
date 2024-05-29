0fd306eca10ddb76dec162907de238745712e7b7
Hugo_Victor <105120915+Hugo-Victorr@users.noreply.github.com>
2024-05-29 18:41:26 -0300

    Adiciona DAOs, views e models

---
+++ PBL Project/Models/PadraoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PBL_Project.Models
+{
+    public class PadraoViewModel
+    {
+        public virtual int Id { get; set; }
+
+    }
+}
---
+++ PBL Project/Models/UnidadeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PBL_Project.Models
+{
+    public class UnidadeViewModel : PadraoViewModel
+    {
+        public string Descricao { get; set; }
+        public int EmpresaId { get; set; }
+        public int EstadoId { get; set; }
+        public DateTime DataFundacao { get; set; }
+
+
+        public string EmpresaNome { get; set; }
+        public string EstadoNome { get; set; }
+    }
+}
