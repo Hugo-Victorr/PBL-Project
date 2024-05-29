ebab83148b81a5589cd87a9cf6f57eadba16d054
Hugo_Victor <105120915+Hugo-Victorr@users.noreply.github.com>
2024-05-29 18:37:10 -0300

    Adiciona root e properties

---
+++ PBL Project/wwwroot/js/site.js
@@ -0,0 +1,92 @@
+﻿function apagarEmpresa(id) {
+    if (confirm('Confirma a exclusão do registro?'))
+        location.href = '/empresa/Delete?id=' + id;
+}
+
+function apagarUnidade(id) {
+    if (confirm('Confirma a exclusão do registro?'))
+        location.href = '/unidade/Delete?id=' + id;
+}
+
+function apagarDispositivo(id) {
+    if (confirm('Confirma a exclusão do registro?'))
+        location.href = '/dispositivo/Delete?id=' + id;
+}
+
+function apagarCategoria(id) {
+    if (confirm('Confirma a exclusão do registro?'))
+        location.href = '/categoria/Delete?id=' + id;
+}
+
+function apagarEstado(id) {
+    if (confirm('Confirma a exclusão do registro?'))
+        location.href = '/estado/Delete?id=' + id;
+}
+
+function exibirImagem() {
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(document.getElementById("Imagem").files[0]);
+    oFReader.onload = function (oFREvent) {
+        document.getElementById("imgPreview").src = oFREvent.target.result;
+    };
+}
+
+//function aplicaFiltroConsultaAvancadaEmpresas() {
+//    var vDescricao = document.getElementById('descricao').value;
+//    var vCategoria = document.getElementById('categoria').value;
+//    var vDataInicial = document.getElementById('dataInicial').value;
+//    var vDataFinal = document.getElementById('dataFinal').value;
+//    $.ajax({
+//        url: "/jogo/ObtemDadosConsultaAvancada",
+//        data: { descricao: vDescricao, categoria: vCategoria, dataInicial: vDataInicial, dataFinal: vDataFinal },
+//        success: function (dados) {
+//            if (dados.erro != undefined) {
+//                alert(dados.msg);
+//            }
+//            else {
+//                document.getElementById('resultadoConsulta').innerHTML = dados;
+//            }
+//        },
+//    });
+//}
+
+//function aplicaFiltroConsultaAvancadaUnidades() {
+//    var vDescricao = document.getElementById('descricao').value;
+//    var vCategoria = document.getElementById('categoria').value;
+//    var vDataInicial = document.getElementById('dataInicial').value;
+//    var vDataFinal = document.getElementById('dataFinal').value;
+//    $.ajax({
+//        url: "/jogo/ObtemDadosConsultaAvancada",
+//        data: { descricao: vDescricao, categoria: vCategoria, dataInicial: vDataInicial, dataFinal: vDataFinal },
+//        success: function (dados) {
+//            if (dados.erro != undefined) {
+//                alert(dados.msg);
+//            }
+//            else {
+//                document.getElementById('resultadoConsulta').innerHTML = dados;
+//            }
+//        },
+//    });
+//}
+
+//function aplicaFiltroConsultaAvancadaDispositivos() {
+//    var vDescricao = document.getElementById('descricao').value;
+//    var vCategoria = document.getElementById('categoria').value;
+//    var vDataInicial = document.getElementById('dataInicial').value;
+//    var vDataFinal = document.getElementById('dataFinal').value;
+//    $.ajax({
+//        url: "/jogo/ObtemDadosConsultaAvancada",
+//        data: { descricao: vDescricao, categoria: vCategoria, dataInicial: vDataInicial, dataFinal: vDataFinal },
+//        success: function (dados) {
+//            if (dados.erro != undefined) {
+//                alert(dados.msg);
+//            }
+//            else {
+//                document.getElementById('resultadoConsulta').innerHTML = dados;
+//            }
+//        },
+//    });
+//}
+
+
+
